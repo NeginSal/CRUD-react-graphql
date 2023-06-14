813770b10387490e20d47c4b12b4b79c281e3338
NeginSal <neginsaljooghi@gmail.com>
2023-06-14 09:21:38 +0330

    remove unnecessary components & added appolo provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    </ApolloProvider>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -12,6 +12,6 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
-app.listen(5000, () => {
+app.listen(5000, () => { 
   console.log('now listening for requests on port 5000');
 });
